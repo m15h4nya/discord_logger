c9b8f0267c7b02c3c086db16d8355cf21723b0cb
m15h4nya <i.am.ganter@gmail.com>
2022-02-11 08:41:58 +0300

    created client instance and test onMessageCreate handler

--- main.go
+++ main.go
@@ -1,5 +1,62 @@
 package main
 
-func main() {
+import (
+	"encoding/json"
+	"github.com/bwmarrin/discordgo"
+	"io"
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
+)
 
+type token struct {
+	Token string
+}
+
+func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
+	if m.Author.ID == s.State.User.ID {
+		return
+	}
+
+	if m.Content == "test" {
+		s.ChannelMessageSend(m.ChannelID, "It's working")
+	}
+}
+
+func main() {
+	token := token{}
+	file, err := os.Open("./config/config.json")
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer file.Close()
+	js, err := io.ReadAll(file)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = json.Unmarshal(js, &token)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	discord, err := discordgo.New("Bot " + token.Token)
+	discord.AddHandler(messageCreate)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = discord.Open()
+	sc := make(chan os.Signal, 1)
+	signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt, os.Kill)
+	<-sc
+	if err != nil {
+		log.Fatal(err)
+	}
 }
