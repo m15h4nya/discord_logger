86b234d65b47c39e1ff0cecf598092ffe6119bfd
m15h4nya <i.am.ganter@gmail.com>
2022-03-10 19:56:02 +0300

    refactored

--- main.go
+++ main.go
@@ -1,12 +1,7 @@
 package main
 
-import "sync"
-
 func main() {
-	wg := sync.WaitGroup{}
 	server := HTTPService{}
 	server.InitService()
-	wg.Add(1)
-	go server.createServer()
-	wg.Wait()
+	server.createServer()
 }
