0ddbbc7e10556a523de96995f5e99b4678c6af2d
m15h4nya <i.am.ganter@gmail.com>
2022-03-10 20:18:54 +0300

    small changes

--- handlers/messageDelete.go
+++ handlers/messageDelete.go
@@ -12,24 +12,24 @@ func (h *Handler) MessageDelete(s *discordgo.Session, m *discordgo.MessageDelete
 		configParser.Contains(m.ChannelID, h.Cfg.IgnoreChannelsIDs) {
 		return
 	}
-	auditLog, err := s.GuildAuditLog(h.Cfg.GuildID, "", "", int(discordgo.AuditLogActionMessageDelete), 100)
-	if err != nil {
-		fmt.Printf("MessageDelete on \"auditLog, err :=...\" : %v\n", err)
-	}
+	//auditLog, err := s.GuildAuditLog(h.Cfg.GuildID, "", "", int(discordgo.AuditLogActionMessageDelete), 100)
+	//if err != nil {
+	//	fmt.Printf("MessageDelete on \"auditLog, err :=...\" : %v\n", err)
+	//}
 	msgAuthor := m.BeforeDelete.Author.Username
 	msgContent := m.BeforeDelete.Content
 	eventAuthor := m.BeforeDelete.Author.Username
 	msgAttachments := m.Attachments
 	msg := fmt.Sprintf("%v: **deleted %v's message** -> %v \nAttachments -> %v", eventAuthor, msgAuthor, msgContent, msgAttachments)
-	for _, entry := range auditLog.AuditLogEntries {
-		if entry.TargetID == m.ID {
-			user, _ := s.User(entry.UserID)
-			eventAuthor = user.Username
-		}
-		fmt.Println(discordgo.SnowflakeTimestamp(entry.TargetID))
-	}
+	//for _, entry := range auditLog.AuditLogEntries {
+	//	if entry.TargetID == m.ID {
+	//		user, _ := s.User(entry.UserID)
+	//		eventAuthor = user.Username
+	//	}
+	//	fmt.Println(discordgo.SnowflakeTimestamp(entry.TargetID))
+	//}
 
-	_, err = s.ChannelMessageSend(h.Cfg.LogChannelID, msg)
+	_, err := s.ChannelMessageSend(h.Cfg.LogChannelID, msg)
 	if err != nil {
 		log.Printf("MessageDelete on \"_, err = s.ChannelMessageSend(...\" : %v\n", err)
 	}
