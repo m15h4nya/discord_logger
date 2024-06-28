6477b864bd376945a2045e4f28c7368faba55e61
m15h4nya <i.am.ganter@gmail.com>
2024-06-28 17:09:03 +0300

    bump

--- bumpBot/main.py
+++ bumpBot/main.py
@@ -1,100 +1,28 @@
+import requests
+import time
 import json
-from time import sleep
-import json
-from time import sleep
-
-from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
-from selenium.common.exceptions import NoSuchElementException
-from locators import Locators
-
+import random
 
 def get_conf() -> dict:
     cfg = json.load(open("config.json"))
     return cfg
 
+cfg = get_conf()
 
-# def init_driver():
-#     options = webdriver.ChromeOptions() 
-#     options.add_argument('--ignore-ssl-errors=yes')
-#     options.add_argument('--ignore-certificate-errors')
-#     return webdriver.Chrome(
-#         service="driver/chromedriver.exe",
-#         options=options
-#     )
+bumps = cfg["bumps_body"]
+header = cfg["bumps_header"]
+baseUrl = "https://discord.com/api/v9"
 
-def init_driver_remote():
-    options = webdriver.ChromeOptions()
-    options.add_argument('--ignore-ssl-errors=yes')
-    options.add_argument('--ignore-certificate-errors')
-    return webdriver.Remote(
-        command_executor="http://selenium:4444",
-        options=options
-    )
+def send_interaction():
+    for bump_body in bumps:
+        response = requests.post(f"{baseUrl}/interactions", json=bump_body, headers=header)
+        time.sleep(random.randint(10, 20))
+        if response.status_code != 204:
+            requests.post(f"{baseUrl}/channels/521272424302641163/messages", json={"data": f"Error bumping: {response.status_code}"}, headers=header)
+            time.sleep(random.randint(10, 20))
 
 
-def check_for_valid_channel(page: webdriver.Chrome, channel_name: str):
-    sleep(5)
-    return channel_name == page.find_element(*Locators.CHANNELNAME).text
-
-
-def skip_modal(page: webdriver.Remote):
-    try:
-        sleep(5)
-        sleep(5)
-        page.find_element(*Locators.SKIP_MODAL).click()
-    except NoSuchElementException:
-        pass
-
-
-def login(page, cfg: dict):
-    page.get("https://discord.com/login")
-    skip_modal(page)
-    page.find_element(*Locators.EMAIL).send_keys(cfg['login'])
-    sleep(5)
-    page.find_element(*Locators.PASSWORD).send_keys(cfg['password'])
-    sleep(5)
-    page.find_element(*Locators.LOGIN).click()
-    sleep(5)
-    sleep(5)
-
-
-def open_page(driver: webdriver.Remote):
-    driver.get("https://discord.com/channels/465780328611708937/521272424302641163")
-    sleep(5)
-
-
-def send_bump(page, text: str):
-    page.find_element(*Locators.INPUTSPAN).send_keys(text)
-    sleep(1)
-    sleep(1)
-    page.find_element(*Locators.INPUTSPAN).send_keys(Keys.ENTER)
-    sleep(1)
-    sleep(1)
-    page.find_element(*Locators.INPUTSPANDELTA).send_keys(Keys.ENTER)
-    sleep(1)
-    sleep(1)
-
-
-
-commands: list[str] = ["/bump", "/like"]
-driver_ = init_driver_remote()
-sleep(15)
-try:
-    counter = 1
-    login(driver_, get_conf())
-    sleep(40) # to pass captcha if needed
-    open_page(driver_)
-    while True:
-        for i in commands:
-            send_bump(driver_, i)
-        sleep(60 * 10) # 10 minutes
-        counter += 1
-        if counter == 10:
-            counter = 1
-            driver_.refresh()
-        sleep(5)
-except Exception as err:
-    driver_.quit()
-    raise err
-    
+while True:
+    send_interaction()
+    time.sleep(60 * 10 * 3) # 30 minutes
+ 
